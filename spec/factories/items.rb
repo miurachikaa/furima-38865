8a7d60a7771cac819e4082a34073e45c2539ddd4
miurachikaa <chikaa719@gmail.com>
2023-02-01 15:21:43 +0900

    モデルの単体テストの実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name                   { 'test' }
+    item_info                   { 'test' }
+    item_category_id            { '2' }
+    item_sales_status_id        { '2' }
+    item_shipping_fee_status_id { '2' }
+    item_prefecture_id          { '2' }
+    item_scheduled_delivery_id  { '2' }
+    item_price                  { '9999999' }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品できる場合' do
+      it '価格が半角英数値であれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない場合' do
+      it 'imageが空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では保存できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品の情報が空では保存できない' do
+        @item.item_info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item info can't be blank")
+      end
+      it 'カテゴリーが空では保存できない' do
+        @item.item_category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category is not a number")
+      end
+      it '商品の状態が空では保存できない' do
+        @item.item_sales_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item sales status is not a number")
+      end
+      it '配送料の負担が空では保存できない' do
+        @item.item_shipping_fee_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item shipping fee status is not a number")
+      end
+      it '発送元の地域が空では保存できない' do
+        @item.item_prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item prefecture is not a number")
+      end
+      it '発送までの日数が空では保存できない' do
+        @item.item_scheduled_delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item scheduled delivery is not a number")
+      end
+      it '価格が空では保存できない' do
+        @item.item_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price can't be blank")
+      end
+      it '価格が￥299より少ない時は保存できない' do
+        @item.item_price = 299
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+      it '価格が￥9999999より多い時は保存できない' do
+        @item.item_price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+      it '価格が全角では登録できない' do
+        @item.item_price = 'あああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is not a number')
+      end
+    end
+  end
 end
