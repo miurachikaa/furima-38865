e815a557b484808bf7e0dfabb312ca6b0d6bf9c1
miurachikaa <chikaa719@gmail.com>
2023-02-07 17:57:10 +0900

    最終修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,8 +51,9 @@ class ItemsController < ApplicationController
   end
 
   def prevent_url
-    return unless @item.user_id == current_user.id || !@item.order.nil?
+    if @item.user_id != current_user.id || @item.order != nil
 
     redirect_to root_path
+    end
   end
 end
